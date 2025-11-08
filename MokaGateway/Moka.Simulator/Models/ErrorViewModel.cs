435c787690fe01c1326d1c02f8b0d135f44fd932
yildtas <yildtas@gmail.com>
2025-11-09 01:40:44 +0300

    initial

---
+++ MokaGateway/Moka.Simulator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Moka.Simulator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
